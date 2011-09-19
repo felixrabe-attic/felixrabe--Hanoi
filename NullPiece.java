20d74e5470bbecdd48b8d4be99572aaab3aa9c72
Felix Rabe <public@felixrabe.net>
2011-09-19 20:14:07 +0200

    Check size passed to Piece constructor

--- NullPiece.java
+++ NullPiece.java
@@ -1,6 +1,6 @@
 /** Special piece that is returned instead of null. */
 public class NullPiece extends Piece {
     public NullPiece() {
-        super(0);
+        super();
     }
 }
--- Piece.java
+++ Piece.java
@@ -2,7 +2,14 @@ public class Piece {
     private int size;
 
     public Piece(int size) {
+        if (size < 1) {
+            throw new IllegalArgumentException("Size must be at least 1.");
+        }
         this.size = size;
+    }
+
+    Piece() {  // called by NullPiece
+        this.size = 0;
     }
 
     public int getSize() {
